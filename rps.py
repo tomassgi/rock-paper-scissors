7c79fef3fd695f6ede9cb3ea869a3968778823df
tomassgi <149562014+tomassgi@users.noreply.github.com>
2024-01-10 10:11:43 +0200

    Update rps.py

--- rps.py
+++ rps.py
@@ -11,4 +11,44 @@ SCISSOR = "2"
 PAPER = "3"
 
 print("Welcome to Rock, Paper, Scissors!")
+import random
 
+options = ["1", "2", "3"]
+
+user_choice = input("Choose ROCK = 1, SCISSOR = 2, or PAPER = 3")
+computer_choice = random.choice(options)
+
+if user_choice == "1" and computer_choice == "1":
+
+    print("Computer picked 1 - Rock, It's a tie!")
+
+elif user_choice == "2" and computer_choice == "2":
+
+    print("Computer picked 2 - Scissors, It's a tie!")
+
+elif user_choice == "3" and computer_choice == "3":
+
+    print("Computer picked 3 - Paper, It's a tie!")
+
+elif user_choice == "1" and computer_choice == "2":
+    print("Computer picked 2 - Scissors, You win!")
+
+elif user_choice == "3" and computer_choice == "1":
+
+    print("Computer picked 1- Rock, You win!")
+
+elif user_choice == "2" and computer_choice == "3":
+
+    print("Computer picked 3 - Paper, You win!")
+
+elif user_choice == "1" and computer_choice == "3":
+
+    print("Computer picked 3 - Paper, Computer wins!")
+
+elif user_choice == "2" and computer_choice == "1":
+
+    print("Computer picked 1 - Rock, Computer wins!")
+
+elif user_choice == "3" and computer_choice == "1":
+
+    print("Computer picked 2 - Scissors, Computer wins!")
